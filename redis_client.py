e8df2ef2fc75e13b107cec731656998bea3418be
frazeui <farhadsiddique5@gmail.com>
2026-08-27 18:47:19 +0500

    Updated Redis Client

--- redis_client.py
+++ redis_client.py
@@ -1,5 +1,6 @@
 import redis
+import os
 
+REDIS_URL=os.getenv("REDIS_URL")
 
-redis_client=redis.Redis(host='redis',port=6379,db=0,decode_responses=True)
-
+redis_client=redis.Redis.from_url(REDIS_URL, decode_responses=True)
